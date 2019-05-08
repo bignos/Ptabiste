9ecf1082ef07ae5d47541aeb90455d75f9ab5f3e
Bignose <bignos@gmail.com>
2019-05-08 19:11:51 +0200

    Initial commit

---
+++ start_venv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+################################################################################
+#	       Script de démarage de l'environement virtuel                    #
+################################################################################
+
+messsage='Launch with ". ./start_venv.sh"'
+
+echo $messsage
+source venv/bin/activate
